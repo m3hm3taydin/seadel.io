1c632d3ca17e497c109639006463fcc159350ee6
m3hm3taydin <github@mehmetaydin.com>
2019-06-22 22:47:21 +0300

    Changes whitenoise configuration on settings.py

--- seadel/settings.py
+++ seadel/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
