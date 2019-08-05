b41e4d06a1ac19705263aa08668db26f62cb6652
m3hm3taydin <github@mehmetaydin.com>
2019-08-05 21:22:25 +0300

    Adds debug steps for pickle load problem on heroku

--- web/pickle_helper.py
+++ web/pickle_helper.py
@@ -6,10 +6,14 @@ from django.contrib.sites.shortcuts import get_current_site
 
 def get_or_save_dataframe(pk):
     document = Document.objects.get(pk=pk)
+    print('document primary key is : {}'.format(str(pk)))
     try:
         if document.updated == 1:
+            print('document setting is updated or this is a new document lets try to reload pickle')
             return reload_pickle(document)
+        print('lets load the pickle')
         df = pd.read_pickle('pickles/cached_dataframe__' + str(pk) + '__.pkl')
+        print('pickle loaded to df')
         return df
     except:
         return reload_pickle(document)
@@ -19,23 +23,32 @@ def reload_pickle(document):
     document_url = document.document_url
     print('document_url : {}'.format(document_url))
     if document.file.name.endswith('.csv'):
+        print('document file name : {}'.format(document.file.name))
+        print('document file name endswith csv')
         if document.row_count == 0:
             df = pd.read_csv(document_url, sep=document.seperator, encoding=document.encoding)
         else:
             df = pd.read_csv(document_url, sep=document.seperator, nrows=document.row_count, encoding=document.encoding)
         document.updated=0
         document.save()
+        print('document saved')
     elif document.file.name.endswith('.xlsx'):
+        print('document file name : {}'.format(document.file.name))
+        print('document file name endswith xlsx')
         #for now only get first sheet of excel
         xl = pd.ExcelFile(document_url)
         df = xl.parse(xl.sheet_names[0])
         document.updated=0
         document.save()
-
+        print('document saved')
     else:
+        print('Error : Not implemented file extension')
         return
     df = fix_column_names(df)
+    print('Fixed Dataframe column names')
+    print('trying to save pickle file')
     df.to_pickle('pickles/cached_dataframe__' + str(document.pk) + '__.pkl')
+    print('saved pickle file')
     return df
 
 def update_dataframe(df):
